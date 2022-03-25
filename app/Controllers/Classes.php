a46fd946a917b625ccd41dfe1e9d2e21b3ec0f6e
renoldiboz <renoldiboz@gmail.com>
2022-03-25 18:31:55 +0700

    add class

---
+++ app/Controllers/Classes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class Classes extends ResourceController
+{
+    
+}
---
+++ app/Database/Seeds/Exams.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Exams extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
