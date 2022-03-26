05750a3ffd1ed1d5b165e8a62eb8f281b1ef3db5
renoldiboz <renoldiboz@gmail.com>
2022-03-26 12:00:17 +0700

    change

--- app/Controllers/Privileges.php
+++ app/Controllers/Privileges.php
@@ -2,18 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Entities\Privileges as EntitiesPrivileges;
+use App\Libraries\StdobjeToArray;
+use App\Models\Privileges as ModelsPrivileges;
+use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
 
 class Privileges extends ResourceController
 {
+    protected $modelName = ModelsPrivileges::class;
+    protected $format    = 'json';
+    use ResponseTrait;
+
+
     /**
      * Return an array of resource objects, themselves in array format
      *
      * @return mixed
      */
+    /**
+     * @OA\Get(
+     *   path="/api/Privileges",
+     *   summary="fleet document",
+     *   description="fleet document",
+     *   tags={"Privileges"},
+     *   @OA\Response(
+     *     response=200, description="ok",
+     *     @OA\JsonContent(
+     *      type="array",
+     *       @OA\Items(ref="#/components/schemas/Privileges")
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=400, description="Bad Request"
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
     public function index()
     {
-        //
+        return $this->respond($this->model->findAll());
     }
 
     /**
@@ -21,19 +49,49 @@ class Privileges extends ResourceController
      *
      * @return mixed
      */
+    /**
+     * @OA\Get(
+     *   path="/api/Privileges/{id}",
+     *   summary="fleet document",
+     *   description="fleet document",
+     *   tags={"Privileges"},
+     *   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *   ), 
+     *   @OA\Response(
+     *     response=200, description="ok",
+     *      @OA\JsonContent(ref="#/components/schemas/Privileges")
+     *   ), 
+     *   @OA\Response(
+     *     response=400, description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *     response=404, description="404 not found",
+     *     @OA\JsonContent(  
+     *      @OA\Property(property="status", type="double",example = 404),
+     *      @OA\Property(property="error", type="double", example = 404),
+     *        @OA\Property(
+     *          property="messages", type="object", 
+     *          @OA\Property(property="error", type="string", example = "not found"),
+     *       )
+     *     )
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
     public function show($id = null)
     {
-        //
-    }
+        $record = $this->model->find($id);
+        if (!$record) {
+            return $this->failNotFound(sprintf(
+                'user with id %d not found',
+                $id
+            ));
+        }
 
-    /**
-     * Return a new resource object, with default properties
-     *
-     * @return mixed
-     */
-    public function new()
-    {
-        //
+        return $this->respond($record);
     }
 
     /**
@@ -41,19 +99,52 @@ class Privileges extends ResourceController
      *
      * @return mixed
      */
+    /**
+     * @OA\Post(
+     *   path="/api/Privileges",
+     *   summary="fleet document",
+     *   description="fleet document",
+     *   tags={"Privileges"},
+    
+     * @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *      @OA\Schema(ref="#/components/schemas/Privileges"),
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=201, description="created",
+     *      @OA\JsonContent(ref="#/components/schemas/Privileges")
+     *   ), 
+     *   @OA\Response(
+     *     response=400, description="Request error",
+     *     @OA\JsonContent(  
+     *      @OA\Property(property="status", type="double",example = 400),
+     *      @OA\Property(property="error", type="double", example = 400),
+     *        @OA\Property(
+     *          property="messages", type="object", 
+     *          @OA\Property(property="error", type="string", example = "not found"),
+     *       )
+     *     )
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
     public function create()
     {
-        //
-    }
+        $data = $this->request->getVar();
+        if ($data == null) {
+            return $this->fail("data null");
+        }
+        $entity = new EntitiesPrivileges();
+        $array = new StdobjeToArray($data);
+        $entity->fill($array->get());
+        if (!$this->model->save($entity)) {
+            return $this->fail($this->model->errors());
+        }
 
-    /**
-     * Return the editable properties of a resource object
-     *
-     * @return mixed
-     */
-    public function edit($id = null)
-    {
-        //
+        return $this->respondCreated($entity, 'post created');
     }
 
     /**
@@ -61,9 +152,63 @@ class Privileges extends ResourceController
      *
      * @return mixed
      */
+    /**
+     * @OA\Put(
+     *   path="/api/Privileges/{id}",
+     *   summary="fleet document",
+     *   description="fleet document",
+     *   tags={"Privileges"},
+     *   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *   ), 
+     *  @OA\RequestBody(
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *      @OA\Schema(ref="#/components/schemas/Privileges"),
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200, description="updated",
+     *      @OA\JsonContent(ref="#/components/schemas/Privileges")
+     *   ), 
+     *   @OA\Response(
+     *     response=400, description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *     response=404, description="404 not found",
+     *     @OA\JsonContent(  
+     *      @OA\Property(property="status", type="double",example = 404),
+     *      @OA\Property(property="error", type="double", example = 404),
+     *        @OA\Property(
+     *          property="messages", type="object", 
+     *          @OA\Property(property="error", type="string", example = "not found"),
+     *       )
+     *     )
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
     public function update($id = null)
     {
-        //
+        $data = $this->request->getVar();
+        if ($data == null) {
+            return $this->fail("data null");
+        }       
+
+        if ($data == null) {
+            return $this->fail("data null");
+        }
+        $entity = new EntitiesPrivileges();
+        $array = new StdobjeToArray($data);
+        $entity->fill($array->get());
+        if (!$this->model->update($id, $entity)) {
+            return $this->fail($this->model->errors());
+        }
+
+        return $this->respondUpdated($data, "updated");
     }
 
     /**
@@ -71,8 +216,62 @@ class Privileges extends ResourceController
      *
      * @return mixed
      */
+    /**
+     * @OA\Delete(
+     *   path="/api/Privileges/{id}",
+     *   summary="fleet document",
+     *   description="fleet document",
+     *   tags={"Privileges"},
+     *   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *   ), 
+     *   @OA\Response(
+     *     response=200, description="ok",
+     *     @OA\JsonContent(  
+     *      @OA\Property(property="status", type="double",example = 200),
+     *      @OA\Property(property="error", type="double", example = null),
+     *        @OA\Property(
+     *          property="messages", type="object", 
+     *          @OA\Property(property="error", type="string", example = "not found"),
+     *       )
+     *     )
+     *   ), 
+     *   @OA\Response(
+     *     response=400, description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *     response=404, description="404 not found",
+     *     @OA\JsonContent(  
+     *      @OA\Property(property="status", type="double",example = 404),
+     *      @OA\Property(property="error", type="double", example = 404),
+     *        @OA\Property(
+     *          property="messages", type="object", 
+     *          @OA\Property(property="error", type="string", example = "Data Deleted"),
+     *       )
+     *     )
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
     public function delete($id = null)
     {
-        //
+        $data = $this->model->find($id);
+        if ($data) {
+            if ($this->model->delete($id)) {
+                $response = [
+                    'status'   => 200,
+                    'error'    => null,
+                    'messages' => [
+                        'success' => 'Data Deleted ' . $id
+                    ]
+                ];
+                return $this->respondDeleted($response);
+            } else
+                return $this->fail("fail delete $id");
+        } else {
+            return $this->failNotFound('No Data Found with id ' . $id);
+        }
     }
 }
