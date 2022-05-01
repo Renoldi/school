1b99f76f2edc77f2e514ea6034a6fc314a54922a
renoldiboz <renoldiboz@gmail.com>
2022-05-01 14:54:34 +0700

    change

--- app/Database/Seeds/Classes.php
+++ app/Database/Seeds/Classes.php
@@ -2,6 +2,8 @@
 
 namespace App\Database\Seeds;
 
+use App\Entities\Classes as EntitiesClasses;
+use App\Models\Classes as ModelsClasses;
 use CodeIgniter\Database\Seeder;
 
 class Classes extends Seeder
@@ -9,28 +11,26 @@ class Classes extends Seeder
     public function run()
     {
         $faker = \Faker\Factory::create();
-        $iat = time();
-        $now = Date('Y-m-d H:i:s', $iat);
+        $model = new ModelsClasses();
+        $entities = new EntitiesClasses();
+
         $data = [
             [
                 'name'  => 'X',
                 'status'  =>  1,
-                'createdAt'  => $now,
-                'updatedAt'  => $now,
             ],
             [
                 'name'  => 'XI',
                 'status'  =>  1,
-                'createdAt'  => $now,
-                'updatedAt'  => $now,
             ],
             [
                 'name'  => 'XII',
                 'status'  =>  1,
-                'createdAt'  => $now,
-                'updatedAt'  => $now,
             ],
         ];
-        $this->db->table('classes')->insertBatch($data);
+        foreach ($data as $data) {
+            $entities->fill($data);
+            $model->save($entities);
+        }
     }
 }
---
+++ app/Database/Seeds/RunSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class RunSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('Classes');
+        $this->call('Departments');
+        // $this->call('Exams');
+        // $this->call('Hoomrooms');
+        $this->call('Privileges');
+        // $this->call('Resultexams');
+        $this->call('Rooms');
+        // $this->call('Scheduleexams');
+        // $this->call('Students');
+        // $this->call('Subjects');
+        // $this->call('Teachers');
+    }
+}
