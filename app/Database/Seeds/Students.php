29f8cdece1cee600283e876e7459f13442357314
renoldiboz <renoldiboz@gmail.com>
2022-05-03 18:36:40 +0700

    change

--- app/Database/Seeds/Students.php
+++ app/Database/Seeds/Students.php
@@ -22,13 +22,13 @@ class Students extends Seeder
                 'nisn' => generateRandomString(9, '0123456789abcdefghijklmnopqrstuvwxyz') . rand(0, 100),
                 'email' => $faker->email(),
                 'name' => $faker->name(),
+                'dob' => $faker->date(),
                 'image' => 'assets/avatar.png',
                 'password' => 'admin123',
                 'classId' => rand(1, 3),
                 'roomId' => rand(1, 7),
                 'gender' => generateRandomString(1, 'mf'),
                 'status' => 1,
-                'dob' => $faker->date(),
                 'privilegeId' => 5,
             ];
 
