dfc06a270a365234bed9f0abc8941f629c74c87c
renoldiboz <renoldiboz@gmail.com>
2022-05-05 10:06:56 +0700

    change

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,16 +31,16 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-//  ['filter' => 'Auth'], 
+
 $routes->get('/', 'Home::index');
 
 $routes->group('api', function ($routes) {
     $routes->post('Teachers/login', 'Teachers::login');
     $routes->post('Students/login', 'Students::login');
     $routes->get('Subjects', 'Subjects::index');
-    $routes->get('Students/paging/(:any)/(:num)/(:num)', 'Students::paging/$1/$2/$3',['filter' => 'Auth']);
+    $routes->get('Students/paging/(:any)/(:num)/(:num)', 'Students::paging/$1/$2/$3', ['filter' => 'Auth']);
 });
-// $routes->group('api', ['filter' => ['Auth','Role:5']], function ($routes) {
+
 $routes->group('api', ['filter' => 'Auth'], function ($routes) {
     $routes->post('Classs/fromXl', 'Classs::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Exams/fromXl', 'Exams::fromXl', ['filter' => 'Role:superadmin,admin']);
@@ -49,22 +49,34 @@ $routes->group('api', ['filter' => 'Auth'], function ($routes) {
     $routes->post('Resultexams/fromXl', 'Resultexams::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Rooms/fromXl', 'Rooms::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Scheduleexams/fromXl', 'Scheduleexams::fromXl', ['filter' => 'Role:superadmin,admin']);
-    // $routes->get('Students/details', 'Students::details', ['filter' => 'Role:1']);
     $routes->post('Students/fromXl', 'Students::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Subjects/fromXl', 'Subjects::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Teachers/fromXl', 'Teachers::fromXl', ['filter' => 'Role:superadmin,admin']);
+    
     $routes->get('Teachers/details', 'Teachers::details', ['filter' => 'Role:superadmin,admin,teacher,editor']);
     $routes->get('Students/details', 'Students::details', ['filter' => 'Role:superadmin,admin,editor,student']);
-    $routes->resource('Classs');
-    $routes->resource('Exams');
-    $routes->resource('Hoomrooms');
-    $routes->resource('Privileges');
-    $routes->resource('Resultexams');
-    $routes->resource('Rooms');
-    $routes->resource('Scheduleexams');
-    $routes->resource('Students');
-    $routes->resource('Subjects');
-    $routes->resource('Teachers');
+
+    $routes->delete('Classs/(:num)', 'Classs::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Exams/(:num)', 'Exams::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Hoomrooms/(:num)', 'Hoomrooms::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Resultexams/(:num)', 'Resultexams::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Rooms/(:num)', 'Rooms::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Scheduleexams/(:num)', 'Scheduleexams::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Students/(:num)', 'Students::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Subjects/(:num)', 'Subjects::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+    $routes->delete('Teachers/(:num)', 'Teachers::delete/$1', ['filter' => 'Role:superadmin,admin,editor']);
+
+    $routes->resource('Classs', ['except' => 'delete']);
+    $routes->resource('Exams', ['except' => 'delete']);
+    $routes->resource('Hoomrooms', ['except' => 'delete']);
+    $routes->resource('Resultexams', ['except' => 'delete']);
+    $routes->resource('Rooms', ['except' => 'delete']);
+    $routes->resource('Scheduleexams', ['except' => 'delete']);
+    $routes->resource('Students', ['except' => 'delete']);
+    $routes->resource('Subjects', ['except' => 'delete']);
+    $routes->resource('Teachers', ['except' => 'delete']);
+
+    $routes->resource('Privileges', ['filter' => 'Role:superadmin,admin']);
 });
 
 /*
