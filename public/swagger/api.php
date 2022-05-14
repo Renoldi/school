28e97540dd0e96e2591e375dde20d85d3a235318
renoldiboz <renoldiboz@gmail.com>
2022-05-14 19:59:10 +0700

    change

--- public/swagger/api.php
+++ public/swagger/api.php
@@ -1,10 +1,18 @@
 <?php 
-$baseUrl = '../../';
-require($baseUrl."vendor/autoload.php"); 
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+ 
+chdir(__DIR__); 
+$pathsConfig = FCPATH . '../../'; 
+
+require realpath($pathsConfig."vendor/autoload.php") ?: $pathsConfig."vendor/autoload.php";
+  
 $openapi = \OpenApi\Generator::scan([
-    $baseUrl.'app/Controllers',
-    $baseUrl.'app/Entities',
-    $baseUrl.'app/Models',
+    $pathsConfig.'app/Controllers',
+    $pathsConfig.'app/Entities',
+    $pathsConfig.'app/Models',
 ]);
 header('Content-Type: application/x-yaml');
 echo $openapi->toYaml();
