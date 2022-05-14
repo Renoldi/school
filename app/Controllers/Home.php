955c7015dc7c5d5d0f29800fdfad1cfc848be009
renoldiboz <renoldiboz@gmail.com>
2022-05-14 20:22:02 +0700

    change

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,10 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    protected $helpers = ['form'];
+    // protected $helpers = ['form'];
     public function index()
     {
-        return view('upload_form',['errors' => []]);
+        // return view('upload_form',['errors' => []]);
         // return view('welcome_message');
+        return redirect()->to('https://man1acehtengah.sch.id'); 
     }
 }
