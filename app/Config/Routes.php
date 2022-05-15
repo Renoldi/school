9e2bc1e9016fc19ac2d0dde3d8d66d89f38cc6d1
renoldiboz <renoldiboz@gmail.com>
2022-05-15 14:04:25 +0700

    change

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,7 +38,6 @@ $routes->group('api', function ($routes) {
     $routes->post('Teachers/login', 'Teachers::login');
     $routes->post('Students/login', 'Students::login');
     $routes->get('Subjects', 'Subjects::index');
-    $routes->post('Teachers/fromXl', 'Teachers::fromXl');
 });
 
 $routes->group('api', ['filter' => 'Auth'], function ($routes) {
@@ -59,7 +58,7 @@ $routes->group('api', ['filter' => 'Auth'], function ($routes) {
     $routes->post('Scheduleexams/fromXl', 'Scheduleexams::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Students/fromXl', 'Students::fromXl', ['filter' => 'Role:superadmin,admin']);
     $routes->post('Subjects/fromXl', 'Subjects::fromXl', ['filter' => 'Role:superadmin,admin']);
-    // $routes->post('Teachers/fromXl', 'Teachers::fromXl', ['filter' => 'Role:superadmin,admin']);
+    $routes->post('Teachers/fromXl', 'Teachers::fromXl', ['filter' => 'Role:superadmin,admin']);
 
     $routes->get('Teachers/details', 'Teachers::details', ['filter' => 'Role:superadmin,admin,teacher,editor']);
     $routes->get('Students/details', 'Students::details', ['filter' => ['Auth','Role:superadmin,admin,editor,student']]);
