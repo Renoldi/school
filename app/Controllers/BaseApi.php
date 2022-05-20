8b7439a109230637f863fa80ed25c803a459ce99
renoldiboz <renoldiboz@gmail.com>
2022-05-20 11:35:11 +0700

    add trim file upload

--- app/Controllers/BaseApi.php
+++ app/Controllers/BaseApi.php
@@ -24,8 +24,8 @@ use CodeIgniter\RESTful\ResourceController;
  */
 class BaseApi extends ResourceController
 {
-     use ResponseTrait; 
-     
+    use ResponseTrait;
+
     /**
      * Return an array of resource objects, themselves in array format
      *
@@ -129,9 +129,10 @@ class BaseApi extends ResourceController
             if ($x == 0) {
                 continue;
             }
-            $subjectEntity->name  = $row[0]; if ($row[0] == '')
+            $subjectEntity->name  = trim($row[0]);
+            if ($row[0] == '')
                 continue;
-            $subjectEntity->statusId = $row[1];
+            $subjectEntity->statusId = trim($row[1]);
 
             if (!$subject->save($subjectEntity)) {
                 return $this->failValidationErrors(
