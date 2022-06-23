48fc9de0a3dd6756673016cbd5e1a950409d788b
renoldiboz <renoldiboz@gmail.com>
2022-06-23 14:46:32 +0700

    change

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -26,7 +26,8 @@ class Cors implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
          
-        header('Access-Control-Allow-Origin: *'); 
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Credentials: true');
         header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
